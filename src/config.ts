33640edd43a814dd1055d83eaad79bf4c7cbfc93
hamdan380 <hamdanashraf2001@gmail.com>
2025-10-17 23:05:14 +0200

    Initial commit

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export const siteConfig = {
-  name: "Ryan Fitzgerald",
-  title: "Senior Software Engineer",
-  description: "Portfolio website of Ryan Fitzgerald",
+  name: "Hamdan Ashraf P",
+  title: "Artificial Intelligence Student",
+  description: "Portfolio website of Hamdan",
   accentColor: "#1d4ed8",
   social: {
-    email: "your-email@example.com",
-    linkedin: "https://linkedin.com/in/yourprofile",
+    email: "hamdanashraf@hotmail.co.uk",
+    linkedin: "https://linkedin.com/in/hamdan-ashraf",
     twitter: "https://x.com/rfitzio",
-    github: "https://github.com/RyanFitzgerald",
+    github: "https://github.com/hamdan-ashraf",
   },
   aboutMe:
     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rem quos asperiores nihil consequatur tempore cupiditate architecto natus commodi corrupti quas quasi facere est, dignissimos odit nam veniam sapiente ut, vitae eligendi ipsum dolor, nostrum ullam impedit! Corrupti ratione mollitia temporibus necessitatibus, consectetur reiciendis recusandae id, dolorum quaerat, vero pariatur. Ratione!",
